d0b73dfc92d2a46af12db9ac7bc363ec9b700612
Alan <ajohnson88@gmail.com>
2015-11-16 14:43:28 -0500

    lots of stuff

---
+++ models/index.js
@@ -0,0 +1,43 @@
+// pull in the Sequelize library
+var Sequelize = require('sequelize');
+// create an instance of a database connection
+// which abstractly represents our app's mysql database
+var twitterjsDB = new Sequelize('twitterjs', 'root', null, {
+    dialect: "mysql",
+    port: 3306,
+});
+
+// open the connection to our database
+twitterjsDB
+    .authenticate()
+    .catch(function(err) {
+        console.log('Unable to connect to the database:', err);
+    })
+    .then(function() {
+        console.log('Connection has been established successfully.');
+    });
+
+var Tweet = require('./tweet')(twitterjsDB);
+var User = require('./user')(twitterjsDB);
+
+// adds a UserId foreign key to the `Tweet` table
+User.hasMany(Tweet);
+Tweet.belongsTo(User);
+
+// User.findOne().then(function (user) {
+//     console.log( user.getTweets()); // big old crazy object, but no name or id anywhere in there
+// });
+
+// User.findOne().then(function (user) {
+//     console.log("NAME: " + user.name); // produces correct result. wat.
+// });
+
+// User.findOne().then(function (user) {
+//     console.log(user.dataValues);
+// });
+
+
+module.exports = {
+    User: User,
+    Tweet: Tweet
+};
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,105 @@
 var express = require('express');
 var router = express.Router();
+// var Tweet = require('../models/tweet.js')(twitterjsDB);
+// var User = require('../models/user.js')(twitterjsDB);
+var User = require('../models/index.js').User;
+var Tweet = require('../models/index.js').Tweet;
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+router.get('/', function(req, res) {
+    // var tweets = tweetBank.list();
+    // res.render('index', {
+    //     title: 'Twitter.js',
+    //     tweets: tweets
+    // });
+
+    //var attributes = [ 'tweet', ['User.name', 'name']]
+    Tweet
+        .findAll({
+            attributes: ['tweet'],
+            include: [{
+                model: User,
+                attributes: ['name']
+            }],
+        }).then(function(tweets) {
+            var mapTweets = tweets.map(function(tweet) {
+                // console.log("TWEE:", tweet);
+                                var newObj = {}
+                newObj.id = tweet.id
+                newObj.name = tweet.User.name;
+                newObj.tweet = tweet.tweet;
+                return newObj;
+            })
+            // console.log("MAPTWEET: " + mapTweets)
+                // console.log("TWEETS: " + JSON.stringify(tweets[0])) ;
+
+            res.render('index', {
+                title: 'Twitter.js',
+                tweets: mapTweets
+            });
+
+
+        });
+
+
 });
 
-function getTweet (req, res){
-  var tweets = tweetBank.find(req.params);
-  res.render('index', { tweets: tweets });
+function getTweet(req, res) {
+
+    User
+        .findOne({
+            where: {
+                name: req.params.name
+            },
+            attributes: ['name'],
+            include: [{
+                model: Tweet,
+                attributes: ['tweet', 'id']
+            }],
+        })
+        .then(function(user) {
+            // console.log("USER: " + JSON.stringify(user))
+            var mapTweets = user.Tweets.map(function(tweet) {
+                var newObj = {}
+                newObj.id = tweet.id
+                newObj.name = user.name;
+                newObj.tweet = tweet.tweet;
+                return newObj;
+            });
+
+            if (req.params.id){
+                 mapTweets = mapTweets.filter(function(tweet){
+                    console.log(tweet, "tweetID: " + tweet.id ,"PARAM_ID: " + req.params.id )
+                    return tweet.id == req.params.id;
+                });
+            }
+            console.log("Mapped: ", mapTweets);
+            res.render('index', {
+                tweets: mapTweets
+            });
+       });
 }
+
+
 
 router.get('/users/:name', getTweet);
 router.get('/users/:name/tweets/:id', getTweet);
 
 // note: this is not very REST-ful. We will talk about REST in the future.
 router.post('/submit', function(req, res) {
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/');
+    var name = req.body.name;
+    var text = req.body.text;
+    var user = User.findOrCreate({
+        where : { name: name }
+    }).then(function(user) {
+        console.log("CREATED USER: " + user)
+       Tweet.create({UserId:user.id, tweet: text});
+
+    });
+
+    // var user = User.create({name:})
+    res.redirect('/');
 });
 
 module.exports = router;
-
-
